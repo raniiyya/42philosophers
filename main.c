202084fce133f8d7ec1c589813e308f32efb7c64
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-02-12 08:17:41 +0000

    edited main.c

--- main.c
+++ main.c
@@ -6,8 +6,23 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:25:26 by rdavurov          #+#    #+#             */
-/*   Updated: 2025/02/10 10:33:20 by codespace        ###   ########.fr       */
+/*   Updated: 2025/02/12 08:17:27 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int		main(int ac, char **av)
+{
+	t_table	table;
+	
+	if (5 == ac || 6 == ac)
+	{
+		parse_input(&table, av); //errors checking, filling table
+		data_init(&table); // creating the thing
+		dinner_start(&table); // starting the simulation
+		clean(&table); // freeing the memory, philo is full or dead
+	}
+	else
+		error_exit("Wrong input");	
+}
