52ef2cd753ce88ce11627af880d2e3e2b075ede2
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-02-12 14:04:52 +0000

    fixed safe_mutex_handle

--- save_functions.c
+++ save_functions.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:55:45 by rdavurov          #+#    #+#             */
-/*   Updated: 2025/02/12 14:01:50 by codespace        ###   ########.fr       */
+/*   Updated: 2025/02/12 14:04:02 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	safe_mutex_handle(t_mtx *mutex, t_opcode opcode)
 	if (LOCK == opcode)
 		pthread_mutex_lock(mutex);
 	else if (UNLOCK == opcode)
-		pthread_mutex_init(mutex);
+		pthread_mutex_unlock(mutex);
 	else if (INIT == opcode)
 		pthread_mutex_init(mutex, NULL);
 	else if (DESTROY == opcode)
