5d4e24c8b2b092f9bcc3fed76b3595fb7ec8dc05
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-02-14 12:26:08 +0000

    corrected table->philo

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:53:28 by rdavurov          #+#    #+#             */
-/*   Updated: 2025/02/12 14:05:10 by codespace        ###   ########.fr       */
+/*   Updated: 2025/02/14 12:25:42 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	data_init(t_table *table)
 {
 	table->dead = false;
-	table->philos = save_malloc(table->philo_count);
+	table->philos = save_malloc(sizeof(t_philo) * table->philo_count);
+	table->forks = save_malloc(sizeof(t_fork) * table->philo_count);
 }
