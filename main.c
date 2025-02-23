e8330148b0ad04a5e7980aeedb2673eb7434e863
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-02-23 16:51:05 +0000

    formated a bit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:25:26 by rdavurov          #+#    #+#             */
-/*   Updated: 2025/02/12 08:17:27 by codespace        ###   ########.fr       */
+/*   Updated: 2025/02/23 16:50:59 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int		main(int ac, char **av)
 {
 	t_table	table;
 	
-	if (5 == ac || 6 == ac)
+	if (ac == 5 || ac == 6)
 	{
 		parse_input(&table, av); //errors checking, filling table
 		data_init(&table); // creating the thing
 		dinner_start(&table); // starting the simulation
-		clean(&table); // freeing the memory, philo is full or dead
+		clean(&table); // freeing the memory, philo is full or end_time
 	}
 	else
 		error_exit("Wrong input");	
