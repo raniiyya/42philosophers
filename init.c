29680394367335aae42c0875d9c0e68bbe9df60d
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-02-23 16:50:44 +0000

    deleted unnecessary lines and fixed bugs

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:53:28 by rdavurov          #+#    #+#             */
-/*   Updated: 2025/02/17 12:43:31 by codespace        ###   ########.fr       */
+/*   Updated: 2025/02/23 16:50:14 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,11 @@ static void	philo_init(t_table *table)
 	while (++i < table->philo_count)
 	{
 		philo = table->philos + i;
-		philo->id = i;
+		philo->id = i + 1;
 		philo->full = false;
 		philo->meals_count = 0;
 		philo->table = table;
-		safe_mutex_handle(philo->philo_mtx, INIT);
+		safe_mutex_handle(&philo->philo_mtx, INIT);
 		assign_forks(philo, table->forks, i);
 	}
 }
@@ -49,15 +49,17 @@ void	data_init(t_table *table)
 	int	i;
 
 	i = -1;
-	table->dead = false;
+	table->end_time = false;
 	table->all_threads_ready = false;
 	table->philos = safe_malloc(sizeof(t_philo) * table->philo_count);
-	safe_mutex_handle(table->table_mtx, INIT);
+	safe_mutex_handle(&table->table_mtx, INIT);
+	safe_mutex_handle(&table->print_mtx, INIT);
 	table->forks = safe_malloc(sizeof(t_fork) * table->philo_count);
 	while (++i < table->philo_count)
 	{
-		safe_mutex_handle(table->forks[i].fork, INIT);
+		safe_mutex_handle(&table->forks[i].fork, INIT);
 		table->forks[i].fork_id = i;
+		
 	}
 	philo_init(table);
 }
